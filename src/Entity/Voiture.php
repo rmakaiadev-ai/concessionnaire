8a2bbe46c552dfde6afa8927595e20eeaa2bfdb5
Randy <rmakaiadevgmail.com>
2026-05-12 15:46:56 +0200

    crud + bdd

---
+++ src/Entity/Voiture.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\VoitureRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: VoitureRepository::class)]
+class Voiture
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $Marque = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $modele = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMarque(): ?string
+    {
+        return $this->Marque;
+    }
+
+    public function setMarque(string $Marque): static
+    {
+        $this->Marque = $Marque;
+
+        return $this;
+    }
+
+    public function getModele(): ?string
+    {
+        return $this->modele;
+    }
+
+    public function setModele(string $modele): static
+    {
+        $this->modele = $modele;
+
+        return $this;
+    }
+}
