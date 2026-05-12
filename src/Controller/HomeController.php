65512db960c3ae3b5c14f4ac8b97bc227bf66475
Randy <rmakaiadevgmail.com>
2026-05-12 13:08:55 +0200

    Modif titre

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,6 +12,7 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        dd(bin2hex(random_bytes(16)));
+        return $this->render('home/index.html.twig',[]);
     }
 }
